0d47fede488c762c06aeaa082fcc49bd6e071905
Chon Lok Lei <chonloklei@gmail.com>
2019-05-09 14:51:56 +0100

    Update plots

--- optimise/plot.py
+++ optimise/plot.py
@@ -7,7 +7,6 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
 
 import model as m
 
@@ -52,28 +51,27 @@ v = model.voltage(times)
 fig = plt.figure(figsize=(16, 6))
 fig.add_subplot(211)
 plt.plot(times, v)
-currentAxis = plt.gca()
 last_t = 0
 for i in range(len(all_p) // 6):
     p = all_p[6 * i:6 * (i + 1)]
     left_x = last_t + np.sum(p[1:-2:2])
     width_x = (np.sum(p[1::2]) - np.sum(p[1:-2:2]))
-    currentAxis.add_patch(Rectangle((left_x, -250), width_x, 500,
-            facecolor="grey", alpha=0.2))
+    plt.axvspan(left_x, left_x + width_x, facecolor="grey", alpha=0.2)
     last_t += np.sum(p[1::2])
+plt.ylabel('Voltage (mV)')
 fig.add_subplot(212)
-# plt.plot(t, d[model.current_list()[max_current]])
+# plt.plot(times, d[model.current_list()[max_current]]) # show specific current
 plt.plot(times, total_c)
-plt.ylim([-1, 2])
-currentAxis = plt.gca()
+plt.ylim([-500, 500])
 last_t = 0
 for i in range(len(all_p) // 6):
     p = all_p[6 * i:6 * (i + 1)]
     left_x = last_t + np.sum(p[1:-2:2])
     width_x = (np.sum(p[1::2]) - np.sum(p[1:-2:2]))
-    currentAxis.add_patch(Rectangle((left_x, -250), width_x, 500,
-            facecolor="grey", alpha=0.2))
+    plt.axvspan(left_x, left_x + width_x, facecolor="grey", alpha=0.2)
     last_t += np.sum(p[1::2])
+plt.ylabel('Current (pA)')
+plt.xlabel('Time (ms)')
 plt.savefig('%s/opt-prt-%s.png' % (savedir, info.save_name))
 
 
@@ -102,7 +100,7 @@ for ii in range(len(all_p) // 6):
 
         dIdvar = (d1[cur][idx_i:idx_f] - d_1[cur][idx_i:idx_f]) \
                 / (2 * sensitivity)
-        p_sensitivity.append(np.sum(np.abs(dIdvar)))
+        p_sensitivity.append(np.sum(np.abs(dIdvar)) / len(dIdvar))
     x = range(len(p_sensitivity))
 
     barlist = axes[ii].bar(x, p_sensitivity)
@@ -113,6 +111,7 @@ for ii in range(len(all_p) // 6):
 
 # plt.xticks(x, [i.split('.')[1] for i in model.current_list()])
 plt.xticks(x, info.parameters, rotation=90)
-axes[len(all_p) // 6 // 2].set_ylabel('Percentage contribution')
+axes[len(all_p) // 6 // 2].set_ylabel('Parameter sensitivity')
+plt.tight_layout()
 plt.savefig('%s/opt-prt-%s-matrix.png' % (savedir, info.save_name))
 
